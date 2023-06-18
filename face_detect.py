331b0dbcca63ff20f3a833109e166cce3eeb5e97
tsuchidashinya <tsuchida.shinya413@mail.kyutech.jp>
2023-06-18 19:21:11 +0900

    fix: log_and_image

--- face_detect.py
+++ face_detect.py
@@ -15,9 +15,11 @@ log_file = open("log.txt", "w")
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 mtcnn = MTCNN(image_size=160, margin=0,  min_face_size=20, thresholds=[0.6, 0.7, 0.7], factor=0.709, post_process=True,  device=device)
 resnet = InceptionResnetV1(pretrained='vggface2').eval().to(device)
-image_paths = ["naoki_1.JPG", "naoki_2.JPG", "shinya_3.JPG", "shinya_2.JPG", "tailor.jpeg", "tailor_1.jpeg", "angelina_1.jpeg", "angelina_2.jpeg", "ul_1.jpeg", "ul.jpeg"]
+image_paths = ["tsuchida_3.JPG", "tsuchida_2.JPG", "mybrother_1.JPG", "mybrother_2.JPG", "angelina_1.jpeg", "angelina_2.jpeg", "will_smith_2.jpeg", "will_smith_1.jpeg"]
+names = []
 embeddings = []
 for i, image_path in enumerate(image_paths):
+    names.append(os.path.splitext(image_path)[0])
     image_path = os.path.join("images", image_path)
     img = Image.open(image_path)
     start = time.time()
@@ -33,5 +35,5 @@ for i, image_path in enumerate(image_paths):
     print(file=log_file)
     embeddings.append(img_probs)
 dists= [[cos_similarity(e1, e2) for e2 in embeddings] for e1 in embeddings]
-print(pd.DataFrame(dists, columns=image_paths, index=image_paths), file=log_file)
+print(pd.DataFrame(dists, columns=names, index=names), file=log_file)
 
